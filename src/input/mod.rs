26ca4cb497572c0e9b1bb1d1bd69bd844f53995d
Tobias Breitwieser <tobias@breitwieser.biz>
2026-01-07 21:06:22 +0100

    delete old src folder after refactor

--- src/input/mod.rs
+++
@@ -1,7 +0,0 @@
-mod parser;
-mod traits;
-mod uart;
-
-pub use parser::{parse, parse_message, ParsedMessage, MAX_LINE_LENGTH};
-pub use traits::{InputError, InputSource};
-pub use uart::UartInputSource;
--- src/output/mod.rs
+++
@@ -1,5 +0,0 @@
-mod traits;
-pub mod usb_hid;
-
-pub use traits::{OutputError, OutputSink};
-pub use usb_hid::{GamepadReport, UsbHidOutput};
